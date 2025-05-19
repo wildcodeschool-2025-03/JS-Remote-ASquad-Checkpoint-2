106dd66247317718bef41ac9921b82f21d4c1fb0
GARY GRAS <gary.gras07@gmail.com>
2025-05-19 10:34:58 +0200

    feat: finished step 2 fetch

--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -1,4 +1,5 @@
 import Cupcake from "../components/Cupcake";
+import { useEffect, useState } from "react";
 
 /* ************************************************************************* */
 const sampleCupcakes: CupcakeArray = [
@@ -36,35 +37,46 @@ const sampleCupcakes: CupcakeArray = [
 /* ************************************************************************* */
 
 function CupcakeList() {
-  // Step 1: get all cupcakes
+  const [cupcakes, setCupcakes] = useState<[]>([]);
 
-  // Step 3: get all accessories
+  useEffect(() => {
+    fetch("http://localhost:3310/api/cupcakes")
+      .then((response) => response.json())
+      .then((data) => setCupcakes(data))
+      .catch(() => {
+        "Erreur lors du chargement";
+      });
+  }, []);
 
-  // Step 5: create filter state
+  console.log(cupcakes)
 
-  return (
-    <>
-      <h1>My cupcakes</h1>
-      <form className="center">
-        <label htmlFor="cupcake-select">
-          {/* Step 5: use a controlled component for select */}
-          Filter by{" "}
-          <select id="cupcake-select">
-            <option value="">---</option>
-            {/* Step 4: add an option for each accessory */}
-          </select>
-        </label>
-      </form>
-      <ul className="cupcake-list" id="cupcake-list">
-        {/* Step 2: repeat this block for each cupcake */}
-        {/* Step 5: filter cupcakes before repeating */}
-        <li className="cupcake-item">
-          <Cupcake data={sampleCupcakes[0]} />
-        </li>
-        {/* end of block */}
-      </ul>
-    </>
-  );
-}
+// Step 3: get all accessories
+
+// Step 5: create filter state
+
+return (
+  <>
+    <h1>My cupcakes</h1>
+    <form className="center">
+      <label htmlFor="cupcake-select">
+        {/* Step 5: use a controlled component for select */}
+        Filter by{" "}
+        <select id="cupcake-select">
+          <option value="">---</option>
+          {/* Step 4: add an option for each accessory */}
+        </select>
+      </label>
+    </form>
+    <ul className="cupcake-list" id="cupcake-list">
+      {/* Step 2: repeat this block for each cupcake */}
+      {/* Step 5: filter cupcakes before repeating */}
+      <li className="cupcake-item">
+        <Cupcake data={sampleCupcakes[0]} />
+      </li>
+      {/* end of block */}
+    </ul>
+  </>
+);
+  }
 
 export default CupcakeList;
