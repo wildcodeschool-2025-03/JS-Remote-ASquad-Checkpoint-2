82bc023bea88aa6368b3136a0057773fc40ee20a
K-Smagghe <kevin.smagghe.pro@gmail.com>
2025-05-19 10:46:53 +0200

    feat: finish algo2

--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -11,8 +11,21 @@ Si n est inférieur ou égal à 0, la fonction doit retourner un tableau vide []
 */
 
 function getFibonacciSequence(size: number): number[] {
-  // Ton code ici !
-  return [0, 1];
+  if (size <= 0) {
+    return [];
+  }
+  if (size === 1) {
+    return [0];
+  }
+
+  const fibonacciSequence = [0, 1];
+  for (let i = 2; i < size; i++) {
+    const dernierchiffre = fibonacciSequence[fibonacciSequence.length - 1];
+    const avantdernierchiffre = fibonacciSequence[fibonacciSequence.length - 2];
+    const prochainchiffre = dernierchiffre + avantdernierchiffre;
+    fibonacciSequence.push(prochainchiffre);
+  }
+  return fibonacciSequence;
 }
 
 export default getFibonacciSequence;
