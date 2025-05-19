0327296d90d7faa3c8df5aea5ee6e9e65cf1281f
MBoussel <mb.boussel@gmail.com>
2025-05-19 10:18:05 +0200

    feat: finished step 2

--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -12,7 +12,16 @@ Si n est inférieur ou égal à 0, la fonction doit retourner un tableau vide []
 
 function getFibonacciSequence(size: number): number[] {
   // Ton code ici !
-  return [0, 1];
+  const fibonacciSequence = [0, 1];
+  if (size <= 0) {
+    return [];
+  }
+  if (size === 1) {
+    return [0];
+  }
+  for (let i = 1; i < size - 1; i++) {
+    fibonacciSequence.push(fibonacciSequence[i - 1] + fibonacciSequence[i]);
+  }
+  return fibonacciSequence;
 }
-
 export default getFibonacciSequence;
