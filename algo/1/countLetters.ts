fc3752f189c03d0a6c9575be5d08cff3cb288444
NICO GARCIA <07nico.garcia@gmail.com>
2025-05-19 09:40:42 +0200

    feat: finished algo1

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -11,7 +11,14 @@ Exemples :
 
 function countLetters(givenString: string, letter: string): number {
   // Ton code ici !
-  return 0;
+  let result = 0;
+
+  for (let i = 0; i <= givenString.length; i++) {
+    if (givenString[i] === letter) {
+      result++;
+    }
+  }
+  return result;
 }
 
 export default countLetters;
